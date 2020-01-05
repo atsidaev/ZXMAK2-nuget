27c2dbef9cf6b7275f7cb6e36598336f210918b8
Alexander Tsidaev <a.tsidaev@gmail.com>
2020-01-06 02:11:36 +0500

    ZXMAK2.Hardware project added, which contains majority of the emulated hardware. Versioning of packages changed to x.x.x.z, where "x.x.x" is the version of package itself and "z" is the version of base ZXMAK2 version (without point symbols).

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+VERSION=1.0.0.2938
 REPO_URL=https://github.com/ZXMAK/ZXMAK2
 BUILD_DIR=.build
+
+NUGET_LOCAL=$PWD/.nuget
+[ -d $NUGET_LOCAL ] || mkdir $NUGET_LOCAL
 
 CPU=ZXMAK2.Engine.Cpu
 CPU_DIR=$BUILD_DIR/src/$CPU
@@ -11,6 +14,17 @@ prepare_directory()
 {
 	rm $1/*.csproj
 	rm -rf $1/Properties
+}
+
+cp_with_version()
+{
+	name=$2
+	if [ -d $name ]
+	then
+		name=$name/`basename $1`
+	fi
+	
+	cat $1 | sed 's/$VERSION/'$VERSION'/' > $name
 }
 
 if [ ! -d $BUILD_DIR ]; then
@@ -24,10 +38,13 @@ fi
 
 rm $BUILD_DIR/src/ZXVM.sln
 prepare_directory $CPU_DIR
-cp $CPU.* $CPU_DIR
+cp_with_version $CPU.csproj $CPU_DIR
+cp_with_version $CPU.nuspec $CPU_DIR
  
 (cd $CPU_DIR; dotnet build . -c Release; nuget pack $CPU.nuspec) || true
 
+## Collect artifacts
+find $CPU_DIR -name '*.nupkg' -exec mv {} $NUGET_LOCAL \;
 
 # Build ZXMAK2.Engine
 
@@ -39,10 +56,10 @@ if [ ! -d $ENGINE_BUILD_DIR ]; then
   mkdir $ENGINE_BUILD_DIR
 fi
 
-cp $ENGINE.csproj $ENGINE_BUILD_DIR
-cp $ENGINE.nuspec $ENGINE_BUILD_DIR
+cp_with_version $ENGINE.csproj $ENGINE_BUILD_DIR
+cp_with_version $ENGINE.nuspec $ENGINE_BUILD_DIR
 
-# Include TimingTool.cs, which we ignored in CPU build
+## Include TimingTool.cs, which we ignored in CPU build
 cp $CPU_DIR/Tools/TimingTool.cs $ENGINE_BUILD_DIR
 
 for proj in $ENGINE_PROJECTS
@@ -51,7 +68,37 @@ do
   mv $BUILD_DIR/src/$proj $ENGINE_BUILD_DIR
 done
 
-# Apply patch for "using" directives
+## Apply patch for "using" directives
 (cd $ENGINE_BUILD_DIR; patch -p1 -i ../../$ENGINE.patch)
 
-(cd $ENGINE_BUILD_DIR; dotnet build . -c Release; nuget pack $ENGINE.nuspec)
+(cd $ENGINE_BUILD_DIR; dotnet restore -s $NUGET_LOCAL; dotnet build . -c Release; nuget pack $ENGINE.nuspec)
+
+## Collect artifacts
+find $ENGINE_BUILD_DIR -name '*.nupkg' -exec mv {} $NUGET_LOCAL \;
+
+# Build ZXMAK2.Hardware
+
+HARDWARE=ZXMAK2.Hardware
+HARDWARE_PROJECTS="ZXMAK2.Hardware ZXMAK2.Hardware.Circuits ZXMAK2.Host.Presentation"
+HARDWARE_BUILD_DIR=$BUILD_DIR/.hardware
+
+if [ ! -d $HARDWARE_BUILD_DIR ]; then
+  mkdir $HARDWARE_BUILD_DIR
+fi
+
+cp_with_version $HARDWARE.csproj $HARDWARE_BUILD_DIR
+cp_with_version $HARDWARE.nuspec $HARDWARE_BUILD_DIR
+
+for proj in $HARDWARE_PROJECTS
+do
+  prepare_directory $BUILD_DIR/src/$proj
+  mv $BUILD_DIR/src/$proj $HARDWARE_BUILD_DIR
+done
+
+## Apply patch for "using" directives
+(cd $HARDWARE_BUILD_DIR; patch -p1 -i ../../$HARDWARE.patch)
+
+(cd $HARDWARE_BUILD_DIR; dotnet restore -s $NUGET_LOCAL; dotnet build . -c Release; nuget pack $HARDWARE.nuspec)
+
+## Collect artifacts
+find $HARDWARE_BUILD_DIR -name '*.nupkg' -exec mv {} $NUGET_LOCAL \;
